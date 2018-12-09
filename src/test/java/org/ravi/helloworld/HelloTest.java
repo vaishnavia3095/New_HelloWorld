66437b2ccff25578d07165cd697c863fe169f3cb
vaishnavia3095 <vaishu.kalpathi@gmail.com>
2018-12-09 10:19:00 +0530

    First change - commenting

--- src/test/java/org/ravi/helloworld/HelloTest.java
+++ src/test/java/org/ravi/helloworld/HelloTest.java
@@ -13,14 +13,14 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 
-public class HelloTest {
+public class HelloTest {/*
 
 	WebDriver driver;
 	WebElement text;
 	@BeforeTest
 	public void beforeTest() {
-		/*	System.setProperty("webdriver.gecko.driver", "resources\\geckodriver32.exe");
-		driver = new FirefoxDriver();*/
+		System.setProperty("webdriver.gecko.driver", "resources\\geckodriver32.exe");
+		driver = new FirefoxDriver();
 		System.setProperty("webdriver.chrome.driver","resources\\chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.get("http://localhost:8080/Helloworld.war/");
@@ -42,4 +42,4 @@ public class HelloTest {
 	}
 
 
-}
+*/}
